a2949b303222a566b10c3a23ba342ecb575a4cbd
itasouza <itasouza@yahoo.com.br>
2018-12-29 20:21:33 -0200

    Criação do Context da Aplicação

---
+++ ProjetoClienteUsandoDDD/src/Projeto.Infrastructure/Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Infrastructure.Data
+{
+    public class Context :DbContext
+    {
+        public Context(DbContextOptions<Context>options) :base(options)
+        {
+
+        }
+    }
+}
