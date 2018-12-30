0a7d2229984726e0ec2c47658e431f8cc5d8e432
itasouza <itasouza@yahoo.com.br>
2018-12-29 23:48:12 -0200

    Criação do método seeding para preenchimento de dados genérico no banco

---
+++ ProjetoClienteUsandoDDD/src/Projeto.Infrastructure/Data/DbInitializer.cs
@@ -0,0 +1,59 @@
+﻿using Projeto.ApplicationCore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projeto.Infrastructure.Data
+{
+   public static class DbInitializer
+    {
+
+        public static void Initialize(AplicacaoContext context)
+        {
+            if (context.Clientes.Any())
+            {
+                return;
+            }
+
+            var clientes = new Cliente[]
+            {
+                new Cliente{
+                    Nome = "José da Silva",
+                    CPF = "25213557040"
+                },
+                new Cliente{
+                    Nome = "Maria da Silva",
+                    CPF = "14110336040"
+                },
+               new Cliente{
+                    Nome = "Vania Maria Souza",
+                    CPF = "93602779084"
+                }
+
+            };
+
+            context.AddRange(clientes);
+
+            var contatos = new Contato[]
+            {
+                new Contato{
+                    Nome = "Contato 01",
+                    Telefone = "11983330842",
+                    Email = "emailcontato1@gmail.com",
+                    Cliente = clientes[0]
+                },
+                new Contato{
+                    Nome = "Contato 02",
+                    Telefone = "119865656",
+                    Email = "emailcontato2@gmail.com",
+                    Cliente = clientes[1]
+                }
+               
+            };
+
+            context.AddRange(contatos);
+            context.SaveChanges();
+        }
+    }
+}
--- ProjetoClienteUsandoDDD/src/Projeto.Web/Program.cs
+++ ProjetoClienteUsandoDDD/src/Projeto.Web/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Projeto.Infrastructure.Data;
 
 namespace Projeto.Web
 {
@@ -14,7 +12,27 @@ namespace Projeto.Web
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<AplicacaoContext>();
+                    context.Database.Migrate();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Um erro ocorreu no método seeding do contexto.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
